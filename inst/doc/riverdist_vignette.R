57a6eaefef8b7c0b2eb5e11dd71334e9413c8997
Tyers <matt.tyers@dfg.alaska.local>
2016-01-21 14:31:10 -0900

    rebuilt vignette

--- inst/doc/riverdist_vignette.R
+++ inst/doc/riverdist_vignette.R
@@ -19,10 +19,11 @@ plot(x=Gulk)
 ## ----fig.width=5,fig.height=5--------------------------------------------
 topologydots(rivers=Gulk)
 
-## ----fig.width=5,fig.height=5--------------------------------------------
+## ----fig.width=5,fig.height=3--------------------------------------------
 data(fakefish)
 fakefish_riv <- xy2segvert(x=fakefish$x, y=fakefish$y, rivers=Gulk)
-head(fakefish_riv)  # a look at what it returned
+head(fakefish_riv)  # a look at the first few rows of the output
+hist(fakefish_riv$snapdist, main="snapping distance (m)")
 
 ## ----eval=FALSE----------------------------------------------------------
 #  segvert_from_shp <- pointshp2segvert(path=".", layer="MyPointShapefile", rivers=MyRivernetwork)
@@ -35,59 +36,49 @@ riverpoints(seg=fakefish_riv$seg, vert=fakefish_riv$vert, rivers=Gulk, pch=15, c
 ## ----fig.width=5,fig.height=5--------------------------------------------
 # starting location: segment 7, vertex 49
 # ending location: segment 14, vertex 121
-riverdistance(startseg=7, startvert=49, endseg=14, endvert=121, rivers=Gulk, map=TRUE)
 detectroute(start=7, end=14, rivers=Gulk)
-
-## ----fig.width=7,fig.height=7--------------------------------------------
-head(fakefish)
-riverdistanceseq(unique=fakefish$fish.id, survey=fakefish$flight, seg=fakefish$seg, 
-                   vert=fakefish$vert, rivers=Gulk)
+riverdistance(startseg=7, startvert=49, endseg=14, endvert=121, rivers=Gulk, map=TRUE)
 
 ## ------------------------------------------------------------------------
-riverdistancematobs(indiv=2, ID=fakefish$fish.id, survey=fakefish$flight,
-      seg=fakefish$seg, vert=fakefish$vert, rivers=Gulk, full=FALSE)
+data(smallset)
+smallset
+riverdistanceseq(unique=smallset$id, survey=smallset$flight, seg=smallset$seg, 
+                   vert=smallset$vert, rivers=Gulk)
 
 ## ------------------------------------------------------------------------
-riverdistancematobs(indiv=1, ID=fakefish$fish.id, survey=fakefish$flight,
-      seg=fakefish$seg, vert=fakefish$vert, rivers=Gulk, full=TRUE)
-
-## ----fig.width=5,fig.height=5--------------------------------------------
-# calculating observed minimum home range for all individuals
-homerange(unique=fakefish$fish.id, seg=fakefish$seg, vert=fakefish$vert, rivers=Gulk)
-
-# calculating and mapping observed minimum home range for individual 15
-homerange(unique=fakefish$fish.id[fakefish$fish.id==15], seg=fakefish$seg[fakefish$fish.id==15],
-           vert=fakefish$vert[fakefish$fish.id==15], rivers=Gulk, map=TRUE)
-
-## ------------------------------------------------------------------------
-logi1 <- fakefish$flight.date==as.Date("2015-11-25")
-riverdistancemat(seg=fakefish$seg, vert=fakefish$vert, rivers=Gulk, logical=logi1)
-
-## ------------------------------------------------------------------------
-streamlocs.seg <- c(1,8,11)
-streamlocs.vert <- c(50,70,90)
-streamlocs.ID <- c("loc A","loc B","loc C")
-
-logi2 <- fakefish$flight.date==as.Date("2015-11-25")
-
-riverdistancetofrom(seg1=streamlocs.seg, vert1=streamlocs.vert, seg2=fakefish$seg, 
-                    vert2=fakefish$vert, ID1=streamlocs.ID, logical2=logi2, rivers=Gulk)
+riverdistancematobs(indiv=1, unique=smallset$id, survey=smallset$flight,
+      seg=smallset$seg, vert=smallset$vert, rivers=Gulk, full=FALSE)
 
 ## ----fig.width=7,fig.height=3.5------------------------------------------
-data(Gulk, fakefish)
-fakefish_sub <- subset(fakefish, flight<=3)
-Gulk_dens <- riverdensity(seg=fakefish_sub$seg, vert=fakefish_sub$vert, rivers=Gulk, 
-  survey=fakefish_sub$flight.date, resolution=2000, bw=10000)
-
+# calculating observed minimum home range for all individuals
 par(mfrow=c(1,3))
-plotriverdensity(riverdensity=Gulk_dens, ramp="blue", dark=0.85, maxlwd=15)
+homerange(unique=smallset$id, seg=smallset$seg, vert=smallset$vert, rivers=Gulk, map=TRUE)
+
+## ------------------------------------------------------------------------
+logi1 <- (smallset$seg==2)
+obsID <- paste0("id",smallset$id,"-flight",smallset$flight)  # constructing observation labels
+riverdistancemat(seg=smallset$seg, vert=smallset$vert, rivers=Gulk, logical=logi1, ID=obsID)
+
+## ------------------------------------------------------------------------
+streamlocs.seg <- c(2,2,2)
+streamlocs.vert <- c(10,20,30)
+streamlocs.ID <- c("loc A","loc B","loc C")
+
+logi2 <- (smallset$seg==2)
+obsID <- paste0("id",smallset$id,"-flight",smallset$flight)
+
+riverdistancetofrom(seg1=streamlocs.seg, vert1=streamlocs.vert, seg2=smallset$seg, 
+                    vert2=smallset$vert, ID1=streamlocs.ID, ID2=obsID, logical2=logi2, 
+                    rivers=Gulk)
 
 ## ----fig.width=5,fig.height=5--------------------------------------------
 showends(seg=1,rivers=Gulk)
 Gulk1 <- setmouth(seg=1, vert=1, rivers=Gulk)
 
-## ----fig.width=7,fig.height=7--------------------------------------------
-riverdistance(startseg=6, endseg=4, startvert=250, endvert=250, rivers=Gulk1, map=TRUE)
+## ----fig.width=5,fig.height=4--------------------------------------------
+zoomtoseg(seg=c(6,3), rivers=Gulk)
+riverpoints(seg=c(6,4), vert=c(250,250), col=4, pch=15, rivers=Gulk1)
+#riverdistance(startseg=6, endseg=4, startvert=250, endvert=250, rivers=Gulk1, map=TRUE)
 text(c(859122.4, 872104.1), c(6964127.4,6969741.0), pos=c(3, 4), labels=c("beginning", "end"))
 riverdirection(startseg=6, endseg=4, startvert=250, endvert=250, rivers=Gulk1)
 upstream(startseg=6, endseg=4, startvert=250, endvert=250, rivers=Gulk1, net=FALSE)
@@ -95,13 +86,59 @@ upstream(startseg=6, endseg=4, startvert=250, endvert=250, rivers=Gulk1, net=TRU
 upstream(startseg=6, endseg=4, startvert=250, endvert=250, rivers=Gulk1, flowconnected=TRUE)
 
 ## ------------------------------------------------------------------------
+data(abstreams)
+
+# distance calculation before defining braiding or building routes
+tstart <- Sys.time()
+riverdistance(startseg=120, startvert=10, endseg=131, endvert=10, rivers=abstreams, 
+              algorithm="Dijkstra")
+Sys.time()- tstart
+
+# distance calculation before building routes
+tstart <- Sys.time()
+riverdistance(startseg=120, startvert=10, endseg=131, endvert=10, rivers=abstreams, 
+              algorithm="sequential")
+Sys.time()- tstart
+
+# distance calculation after building routes
+tstart <- Sys.time()
+riverdistance(startseg=120, startvert=10, endseg=131, endvert=10, rivers=abstreams, 
+              algorithm="segroutes")
+Sys.time()- tstart
+
+## ----eval=FALSE----------------------------------------------------------
+#  data(Gulk, fakefish)
+#  fakefish_density <- riverdensity(seg=fakefish$seg, vert=fakefish$vert, rivers=Gulk,
+#    survey=fakefish$flight.date, resolution=2000, bw=10000)
+#  par(mfrow=c(3,3))
+#  plotriverdensity(riverdensity=fakefish_density, ramp="blue", dark=0.85, maxlwd=15,
+#                   whichplots=c(1:8,10))   # showing only nine plots for clarity
+
+## ----fig.width=7.5,fig.height=8.5,echo=FALSE, eval=FALSE-----------------
+#  data(fakefish_density)
+#  par(mfrow=c(3,4))
+#  plotriverdensity(riverdensity=fakefish_density, ramp="blue", dark=0.85, maxlwd=15)
+
+## ----fig.width=7.5,fig.height=10,echo=FALSE------------------------------
+data(fakefish_density)
+par(mfrow=c(3,3))
+plotriverdensity(riverdensity=fakefish_density, ramp="blue", dark=0.85, maxlwd=15,
+                 whichplots=c(1:8,10))
+
+## ----fig.width=5,fig.height=4--------------------------------------------
+x <- homerange(unique=fakefish$flight.date, seg=fakefish$seg, vert=fakefish$vert, rivers=Gulk)
+x
+plot(x$range, type='b', xaxt='n', xlab="", ylab="range (m)", cex.axis=.6)
+axis(1,at=1:10,labels=sort(unique(fakefish$flight.date)), cex.axis=.6, las=3)
+
+## ------------------------------------------------------------------------
 mouthdist(seg=fakefish$seg[1], vert=fakefish$vert[1], rivers=Gulk)
-x <- mouthdistobs(unique=fakefish$fish.id, survey=fakefish$flight.date,
+x <- mouthdistobs(unique=fakefish$fish.id, survey=fakefish$flight,
     seg=fakefish$seg, vert=fakefish$vert, rivers=Gulk)
 
-head(x)
+round(x)
 
-## ----fig.width=7.5,fig.height=4------------------------------------------
+## ----fig.width=7.5,fig.height=5------------------------------------------
 x <- mouthdistobs(unique=fakefish$fish.id, survey=fakefish$flight.date,
     seg=fakefish$seg, vert=fakefish$vert, rivers=Gulk)
 
@@ -110,129 +147,120 @@ plotseq(seqobs=x)
 plotseq(seqobs=x, type="dotline")
 par(mfrow=c(1,1))
 
-## ----fig.width=7,fig.height=7--------------------------------------------
-data(abstreams)
-plot(x=abstreams)
-riverpoints(seg=c(120,131), vert=c(10, 10), rivers=abstreams, pch=15, col=2)
+## ----fig.width=7.5,fig.height=5------------------------------------------
+x <- upstreamseq(unique=fakefish$fish.id, survey=fakefish$flight, seg=fakefish$seg,
+                 vert=fakefish$vert, rivers=Gulk)
+round(x)
+par(mfrow=c(1,2))
+plotseq(seqobs=x)
 
-abstreams_nosegroutes <- abstreams
-abstreams_nosegroutes$segroutes <- NULL # removing the segment routes for demonstration
-abstreams_nosegroutes0 <- abstreams_nosegroutes
-abstreams_nosegroutes0$braided <- NA   # removing the braiding information for demonstration
+plotseq(seqobs=x, type="dotplot")
+abline(h=0, lty=3)
 
-# distance calculation before defining braiding or building routes
-tstart <- Sys.time()
-riverdistance(startseg=120, startvert=10, endseg=131, endvert=10, rivers=abstreams_nosegroutes0)
-tend <- Sys.time()
-tend - tstart
-
-# distance calculation before building routes
-tstart <- Sys.time()
-riverdistance(startseg=120, startvert=10, endseg=131, endvert=10, rivers=abstreams_nosegroutes)
-tend <- Sys.time()
-tend - tstart
-
-# distance calculation after building routes
-abstreams_segroutes <- buildsegroutes(abstreams_nosegroutes)
-tstart <- Sys.time()
-riverdistance(startseg=120, startvert=10, endseg=131, endvert=10, rivers=abstreams_segroutes)
-tend <- Sys.time()
-tend - tstart
+## ----fig.width=5,fig.height=10-------------------------------------------
+plotupstreammatobs(unique=fakefish$fish.id, survey=fakefish$flight, seg=fakefish$seg, 
+                   vert=fakefish$vert, rivers=Gulk)
 
 ## ----eval=FALSE----------------------------------------------------------
 #  data(abstreams0)  # a messy river network
 #  abstreams_fixed <- cleanup(abstreams0)  # fixing many problems
 
-## ----fig.width=7,fig.height=7--------------------------------------------
-data(Kenai1)
-plot(x=Kenai1)
+## ----fig.width=7.5,fig.height=4, warning=FALSE---------------------------
+Gulk_trim1 <- trimriver(trim=c(10,11,12,13,14), rivers=Gulk)
+Gulk_trim2 <- trimriver(trimto=c(10,11,12,13,14), rivers=Gulk)
 
-Kenai1.trim <- trimriver(trim=c(46,32,115,174,169,114,124,142,80), rivers=Kenai1)
-plot(x=Kenai1.trim)
-
-## ----fig.width=7,fig.height=7--------------------------------------------
-plot(x=Kenai1)
+par(mfrow=c(1,3))
+plot(x=Gulk, main="original")
+plot(x=Gulk_trim1, main="trim=c(10,11,12,13,14)")
+plot(x=Gulk_trim2, main="trimto=c(10,11,12,13,14)")
 
 ## ----fig.width=5,fig.height=5--------------------------------------------
-Kenai1.trim.2 <- trimriver(trimto=c(20,57,118,183,45,162,39,98,19), rivers=Kenai1)
-plot(x=Kenai1.trim.2)
-
-## ----fig.width=7,fig.height=7--------------------------------------------
 data(Kenai3)
 x <- c(174185, 172304, 173803, 176013)
 y <- c(1173471, 1173345, 1163638, 1164801)
+Kenai3.buf1 <- trimtopoints(x=x, y=y, rivers=Kenai3, method="snap")
+Kenai3.buf2 <- trimtopoints(x=x, y=y, rivers=Kenai3, method="snaproute")
+Kenai3.buf3 <- trimtopoints(x=x, y=y, rivers=Kenai3, method="buffer", dist=5000)
 
-plot(Kenai3)
+plot(x=Kenai3, main="original")
 points(x, y, pch=15, col=4)
 legend(par("usr")[1], par("usr")[4], legend="points to buffer around", pch=15, col=4, cex=.6)
 
-## ----fig.width=5,fig.height=5--------------------------------------------
-Kenai3.buf1 <- trimtopoints(x=x, y=y, rivers=Kenai3, method="snap")
-plot(x=Kenai3.buf1)
+## ----fig.width=7.5,fig.height=3.5----------------------------------------
+
+par(mfrow=c(1,3))
+plot(x=Kenai3.buf1, main="snap")
+points(x, y, pch=15, col=4)
+plot(x=Kenai3.buf2, main="snaproute")
+points(x, y, pch=15, col=4)
+plot(x=Kenai3.buf3, main="buffer, dist=5000")
 points(x, y, pch=15, col=4)
 
-## ----fig.width=5,fig.height=5--------------------------------------------
-Kenai3.buf2 <- trimtopoints(x=x, y=y, rivers=Kenai3, method="snaproute")
-plot(x=Kenai3.buf2)
-points(x, y, pch=15, col=4)
+## ----fig.width=7.5,fig.height=5------------------------------------------
+data(Koyukuk2)
+Koy_subset <- trimriver(trimto=c(30,28,29,3,19,27,4),rivers=Koyukuk2)
+Koy_subset <- setmouth(seg=1,vert=427,rivers=Koy_subset)
 
-## ----fig.width=5,fig.height=5--------------------------------------------
-Kenai3.buf3 <- trimtopoints(x=x, y=y, rivers=Kenai3, method="buffer", dist=5000)
-plot(x=Kenai3.buf3)
-points(x, y, pch=15, col=4)
+Koy_subset_trim <- removeunconnected(Koy_subset)
 
-## ----fig.width=5,fig.height=5--------------------------------------------
-data(Kenai1)
-Kenai1.1 <- dissolve(Kenai1)
-Kenai1.1 <- setmouth(seg=63,vert=40,rivers=Kenai1.1)
+par(mfrow=c(1,2))
+plot(x=Koy_subset, main="original")
+plot(x=Koy_subset_trim, main="unconnected segments removed")
 
-Kenai1.2 <- removeunconnected(Kenai1.1)
-plot(Kenai1.2)
-
-## ----fig.width=7,fig.height=7--------------------------------------------
+## ----fig.width=7.5,fig.height=5, warning=FALSE---------------------------
 data(Kenai2)
-plot(x=Kenai2)
+Kenai2_sub <- trimriver(trimto=c(26,157,141,69,3,160,2,35,102,18,64,86,49,103,61
+                                 ,43,183,72,47,176), rivers=Kenai2)
 
-Kenai2.dissolve <- dissolve(rivers=Kenai2)
-plot(x=Kenai2.dissolve)
+Kenai2_sub_dissolve <- dissolve(rivers=Kenai2_sub)
 
-## ----fig.width=7,fig.height=7--------------------------------------------
+par(mfrow=c(1,2))
+plot(x=Kenai2_sub, main="original")
+plot(x=Kenai2_sub_dissolve, main="dissolved")
+
+## ----fig.width=7.5,fig.height=4.5, warning=FALSE-------------------------
 data(Koyukuk1)
-topologydots(rivers=Koyukuk1)
 
 Koyukuk1.split <- splitsegments(rivers=Koyukuk1)
-topologydots(rivers=Koyukuk1.split)
 
-## ----fig.width=5,fig.height=5--------------------------------------------
+par(mfrow=c(1,2))
+topologydots(rivers=Koyukuk1, main="original")
+topologydots(rivers=Koyukuk1.split, main="split")
+
+## ----fig.width=7.5,fig.height=4, warning=FALSE---------------------------
 data(Koyukuk0)
-plot(Koyukuk0, ylim=c(1930500,1931500), xlim=c(194900,195100))
-topologydots(Koyukuk0, add=TRUE)
 
 Koyukuk0.1 <- connectsegs(connect=21, connectto=20, rivers=Koyukuk0)
-plot(Koyukuk0.1,ylim=c(1930500,1931500), xlim=c(194900,195100))
+
+par(mfrow=c(1,2))
+plot(Koyukuk0, ylim=c(1930500,1931500), xlim=c(194900,195100), main="original")
+topologydots(Koyukuk0, add=TRUE)
+
+plot(Koyukuk0.1,ylim=c(1930500,1931500), xlim=c(194900,195100), main="connected")
 topologydots(Koyukuk0.1, add=TRUE)
 
 ## ----eval=FALSE----------------------------------------------------------
 #  data(abstreams1)
 #  abstreams2 <- removemicrosegs(abstreams1)
 
-## ----fig.width=5,fig.height=5--------------------------------------------
+## ----fig.width=7.5,fig.height=4, warning=FALSE---------------------------
 data(Kenai3)
-zoomtoseg(seg=c(73,70,71), rivers=Kenai3)
+Kenai3split <- addverts(Kenai3, mindist=200)
+
+par(mfrow=c(1,2))
+zoomtoseg(seg=c(73,70,71), rivers=Kenai3, main="segment 71 vertices")
 points(Kenai3$lines[[71]]) 
 
-## ----fig.width=5,fig.height=5--------------------------------------------
-Kenai3split <- addverts(Kenai3, mindist=200)
-zoomtoseg(seg=c(73,70,71), rivers=Kenai3split)
+zoomtoseg(seg=c(73,70,71), rivers=Kenai3split, main="adding points every 200m")
 points(Kenai3split$lines[[71]])  
 
-## ----fig.width=5,fig.height=5--------------------------------------------
-data(Gulk)
-plot(x=Gulk)
-checkbraided(rivers=Gulk)
+## ----fig.width=7.5,fig.height=5------------------------------------------
+data(Gulk, KilleyW)
+par(mfrow=c(1,2))
+plot(x=Gulk, main="Gulkana River")
+plot(x=KilleyW, ylim=c(1164500, 1168500), main="Killey River West")
 
-data(KilleyW)
-plot(x=KilleyW, ylim=c(1164500, 1168500))
+checkbraided(rivers=Gulk)
 checkbraided(rivers=KilleyW)
 
 ## ----fig.width=5,fig.height=5--------------------------------------------
@@ -247,11 +275,16 @@ Killey.dists <- riverdistancelist(startseg=1,endseg=16,startvert=25,endvert=25,
    rivers=KilleyW,reps=1000)
 Killey.dists  # 18 routes are detected.
 
-## ----fig.width=5,fig.height=5--------------------------------------------
-plot(x=KilleyW, ylim=c(1164500, 1168500))
+## ----fig.width=7.5,fig.height=5------------------------------------------
+par(mfrow=c(1,2))
+plot(x=KilleyW, ylim=c(1164500, 1168500), main="shortest route")
 riverdistance(startvert=25, endvert=25, path=Killey.dists$routes[[1]], 
               rivers=KilleyW, map=TRUE, add=TRUE)
-plot(KilleyW, ylim=c(1164500, 1168500))
+plot(KilleyW, ylim=c(1164500, 1168500), main="longest route")
 riverdistance(startvert=25, endvert=25, path=Killey.dists$routes[[18]], 
               rivers=KilleyW, map=TRUE, add=TRUE)
+
+## ------------------------------------------------------------------------
+detectroute(start=1, end=16, rivers=KilleyW)
+Killey.dists$routes[[1]]  #calculated above
 
